1c27d81ee4a4e5c66942494507ba7f9876ea7f6b
gbrllcavichion <cavichionmichel@gmail.com>
2024-09-11 22:04:41 -0300

    :bug: Trying to get events from event but having timed out

--- src/main/java/com/api/show_finder/ShowFinderApplication.java
+++ src/main/java/com/api/show_finder/ShowFinderApplication.java
@@ -1,14 +1,23 @@
 package com.api.show_finder;
 
+import com.api.show_finder.client.EventimScraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.io.IOException;
 
 @EnableFeignClients
 @SpringBootApplication
 public class ShowFinderApplication {
 
 	public static void main(String[] args) {
+		EventimScraper scraper = new EventimScraper();
+		try {
+			scraper.scrapeEventimShows();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(ShowFinderApplication.class, args);
 	}
 
