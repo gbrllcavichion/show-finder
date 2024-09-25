78802d2ae1092086a4970fb8c91eeb5b2a7da657
gbrllcavichion <cavichionmichel@gmail.com>
2024-09-24 21:11:36 -0300

    :sparkles: Fixing login

--- src/main/java/com/api/show_finder/configuration/JwtTokenProvider.java
+++ src/main/java/com/api/show_finder/configuration/JwtTokenProvider.java
@@ -2,33 +2,34 @@ package com.api.show_finder.configuration;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.SignatureException;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
-    @Value("${spring.jwt.secret}")
-    private String jwtSecret;
+    private final SecretKey jwtSecret = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
-    private final long jwtExpirationMs = 86400000;
+    private final long jwtExpirationMs = 86400000;  // 24 horas
 
     public String generateToken(Authentication authentication) {
         return Jwts.builder()
                 .setSubject(authentication.getName())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(jwtSecret, SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
